eb76e76a4d6756f095795004b23d0621d05959e5
Diego Beltrán <diegoandreseffie@hotmail.com>
2022-10-12 19:26:49 -0500

    Implementacion de la anotacion Service

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -1,7 +1,11 @@
 package com.platzi.market.persistence;
 
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import com.platzi.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,63 +14,57 @@ import java.util.Optional;
 //Con Repository indicamos que esta clase interactua con la BD, ya que
 //aqui hacemos operaciones que aplicaran en nuestras tablas de la BD
 @Repository
-public class ProductoRepository {
+public class ProductoRepository  implements ProductRepository {
 
     //Atributo creado a partir de la interfaz ProductoCrudRepository
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
+    private ProductMapper mapper;
 
     //Metodo para obtener todos los productos de la BD
-    public List<Producto> getAll(){
+    @Override
+    public List<Product> getAll(){
         /*
            usando la varible previamente creada usamos el metodo
            de la interfaz ProductoCrudRepository findAll()
            para obtener todos los registros
          */
-        return (List<Producto>) productoCrudRepository.findAll();
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
-    /*
-    Metodo para obtener los productos segun su categoria
-    Recibe como parametro el idCategoria(Variable de la clase Producto)
-     */
-    public List<Producto> getByCategoria(int idCategoria){
-        /*
-        Usando la variable creada hacemos llamado del metodo
-        findByIdCategoriaOrderByNombreAsc(idCategoria)
-        pasando como parametro el
-        idCategoria(Variable de la clase producto)
-         */
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
-    /*
-    Metodo para obtener los productos escasos y su estado
-    Recibe como parametro cantidad(Variable de la clase Producto)
-     */
-    public Optional<List<Producto>> getEscasos(int cantidad){
-        /*
-        Usando la variable creada hacemos llamado del metodo
-        findByCantidadStockLessThanAndEstado(cantidad,true)
-        pasando como parametro cantidad(Variable de la clase producto)
-        y true diciendo que se obtendran los productos que son escasos
-         */
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity,true);
+        return productos.map(prods -> mapper.toProducts(prods));
+
     }
 
-    //Metodo para obtener un producto segun el id dado
-    public Optional<Producto> getProducto(int idProducto){
-        return productoCrudRepository.findById(idProducto);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto ->mapper.toProduct(producto));
+
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
 
-    //Metodo para guardar un producto, recibe el producto como parametro
-    public Producto save(Producto producto){
-       return productoCrudRepository.save(producto);
-    }
 
     //Metodo para borrar un producto segun su idProducto
-    public void delete(int idProducto){
-        productoCrudRepository.deleteById(idProducto);
+    @Override
+    public void delete(int productId){
+        productoCrudRepository.deleteById(productId);
     }
 
 
