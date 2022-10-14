96bcfd1990f7b247eda9d7a5dfb761e7f1c0b36d
Diego Beltrán <diegoandreseffie@hotmail.com>
2022-10-14 17:34:05 -0500

    Adicion de dependencia de Jwt, DTO de autenticacion de response y request, ademas del generador de tokens en el paquete security

---
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
@@ -0,0 +1,23 @@
+package com.platzi.market.domain.dto;
+
+public class AuthenticationRequest {
+
+    private String usename;
+    private String password;
+
+    public String getUsename() {
+        return usename;
+    }
+
+    public void setUsename(String usename) {
+        this.usename = usename;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
